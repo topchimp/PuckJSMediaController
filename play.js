d55e3967497c41cacafb497f71b9689516266cdd
Scott Davies <sdavies@thoughtworks.com>
2017-04-01 17:31:47 +0100

    Took out back

--- play.js
+++ play.js
@@ -2,13 +2,6 @@ var controls = require("ble_hid_controls");
 
 NRF.setServices(undefined, { hid : controls.report });
 
-function goBack(){
-// Use the LEFT ARROW key to go to the next slide
-  kb.tap(kb.KEY.LEFT, 0);
-  LED2.set();
-  setTimeout("LED2.reset()",1000);
-}
-
 function play(){
   controls.playpause();
   LED1.set();
